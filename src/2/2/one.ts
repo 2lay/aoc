13d0424d994e59553dda49ac47a71a128d7dd565
2lay <mail@2lay.net>
2024-12-02 15:25:24 +0100

    feat: day 2 puzzle 1

--- src/2/2/one.ts
+++
@@ -1,3 +0,0 @@
-if (import.meta.main) {
-    console.log("Hello, World!");
-}
--- src/2/one.ts
+++ src/2/one.ts
@@ -1,3 +1,12 @@
 if (import.meta.main) {
-    console.log("Hello, World!");
+    const lines = (await Deno.readTextFile("./src/2/data.txt")).trim().split("\n");
+    let correct = 0;
+
+    for (const line of lines) {
+        const numbers = line.split(" ").map(Number);
+        const valid = (isInc) => numbers.every((num, index) => index === 0 || [1, 2, 3].includes((isInc ? num - numbers[index - 1] : numbers[index - 1] - num)));
+        if (valid(true) || valid(false)) correct++;
+    }
+
+    console.log("result: " + correct);
 }
